a44fff569f3e2cc9947be7154ca2cbe9b7b22a0c
Federico <fedeisa195@gmail.com>
2023-02-15 16:52:41 -0300

    Primer Commit

---
+++ useCounter.js
@@ -0,0 +1,29 @@
+import { useState } from "react";
+
+export const useCounter = ( initialValue = 10) => {
+
+    const [counter, setcounter] = useState( initialValue )
+
+    const increment = ( value = 1 ) => {
+        setcounter( (current) => current + value );
+    }
+
+    const decrement = ( value = 1) => {
+        setcounter( (current) => current - value); 
+    }
+
+    const reset = () => {
+        setcounter( initialValue );
+    }
+
+
+    return {
+        counter,
+        increment,
+        decrement,
+        reset,
+    }
+
+}
+
+
